279f740f7f23227385f8e522c479a56f85b526d4
JR Oakes <jroakes@gmail.com>
2021-07-29 09:58:07 -0400

    Updated train script -fresh-opt

--- run_train_test.sh
+++ run_train_test.sh
@@ -7,6 +7,6 @@ pip install -r requirements.txt
 pip install jax==0.2.12
 wget https://storage.googleapis.com/gpt-j-finetuning-europe/zoominfo_test.csv
 
-python3 device_train.py --config=configs/company_data.json --tune-model-path=gs://gpt-j-finetuning-europe/step_383500/ &
+python3 device_train.py --config=configs/company_data.json --tune-model-path=gs://gpt-j-finetuning-europe/step_383500/ --fresh-opt &
 wait
-python3 device_sample_df.py --config=configs/company_data.json --sample_file zoominfo_test.csv --prompt_column model_input --num_samples 25 --temp 0.3
+python3 device_sample_df.py --config=configs/company_data.json --sample_file zoominfo_test.csv --prompt_column model_input --num_samples 25 --temp 0.6
