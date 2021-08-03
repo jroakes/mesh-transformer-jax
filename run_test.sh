c5fbd8f902550d13416a57e895b90e2e52c766e5
JR Oakes <jroakes@gmail.com>
2021-08-02 23:46:52 -0400

    Updated bash scripts

---
+++ run_test.sh
@@ -0,0 +1,15 @@
+
+echo "Removing Old Folder"
+rm -rf mesh-transformer-jax || true
+echo "Cloning new repo"
+git clone https://github.com/jroakes/mesh-transformer-jax.git
+echo "Changing directory"
+cd mesh-transformer-jax
+# pip install -r requirements.txt
+# pip install jax==0.2.12
+
+echo "Grabbing new test file"
+wget https://storage.googleapis.com/gpt-j-finetuning-europe/zoominfo_test.csv
+
+echo "Running sampling script"
+python3 device_sample_df.py --config=configs/company_data.json --sample_file zoominfo_test.csv --prompt_column model_inpu>
--- run_train_test.sh
+++ run_train_test.sh
@@ -1,12 +1,19 @@
 
+echo "Removing Old Folder"
 rm -rf mesh-transformer-jax || true
-
+echo "Cloning new repo"
 git clone https://github.com/jroakes/mesh-transformer-jax.git
+echo "Changing directory"
 cd mesh-transformer-jax
-pip install -r requirements.txt
-pip install jax==0.2.12
+
+#pip install -r requirements.txt
+#pip install jax==0.2.12
+
+echo "Grabbing new test file"
 wget https://storage.googleapis.com/gpt-j-finetuning-europe/zoominfo_test.csv
 
+echo "Running training script"
 python3 device_train.py --config=configs/company_data.json --tune-model-path=gs://gpt-j-finetuning-europe/step_383500/ --fresh-opt &
 wait
+echo "Running sampling script"
 python3 device_sample_df.py --config=configs/company_data.json --sample_file zoominfo_test.csv --prompt_column model_input --num_samples 25 --temp 0.6
