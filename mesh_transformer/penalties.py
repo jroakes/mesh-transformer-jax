31f6764145e974e93270c54f4264c3459e689af5
JR Oakes <jroakes@gmail.com>
2021-08-03 16:36:30 -0400

    updated repetition

--- mesh_transformer/penalties.py
+++ mesh_transformer/penalties.py
@@ -2,21 +2,18 @@ import jax
 import jax.numpy as jnp
 
 
-def _create_next_token_logits_penalties(input_ids, logits, repetition_penalty=None):
-
-    logit_penalties = jnp.ones(logits.shape)
+def _create_next_token_logits_penalties(input_ids, logits, repetition_penalty):
 
     print('input_ids', input_ids)
     print('logits', logits)
     print('Repetiton Penalty', repetition_penalty)
 
-    if repetition_penalty is not None:
-        prev_input_ids = jnp.unique(input_ids) # [[123,234,123,...]]
-        logit_penalized = logits[:, prev_input_ids]
-        logit_penalties = jnp.zeros(logit_penalized.shape)
-        # if previous logit score is < 0 then multiply repetition penalty else divide
-        logit_penalties[:, logit_penalized < 0] = repetition_penalty
-        logit_penalties[:, logit_penalized > 0] = 1 / repetition_penalty
+    prev_input_ids = jnp.unique(input_ids) # [[123,234,123,...]]
+    logit_penalized = logits[:, prev_input_ids]
+    logit_penalties = jnp.zeros(logit_penalized.shape)
+    # if previous logit score is < 0 then multiply repetition penalty else divide
+    logit_penalties[:, logit_penalized < 0] = repetition_penalty
+    logit_penalties[:, logit_penalized > 0] = 1 / repetition_penalty
 
     return logit_penalties
 
@@ -25,6 +22,7 @@ def repetition_penalty(input_ids, logits, options):
 
     repetition_penalty = options.get('repetition_penalty', None)
 
-    penalties = _create_next_token_logits_penalties(input_ids, logits, repetition_penalty)
-    logits = jnp.multiply(logits, penalties)
+    if repetition_penalty is not None:
+        penalties = _create_next_token_logits_penalties(input_ids, logits, repetition_penalty)
+        logits = jnp.multiply(logits, penalties)
     return logits
