1acb84daa4873c40f96e0754d7acdb3a4a9d6026
JR Oakes <jroakes@gmail.com>
2021-08-04 09:21:28 -0400

    penalty change

--- mesh_transformer/penalties.py
+++ mesh_transformer/penalties.py
@@ -65,13 +65,13 @@ def repetition_penalty(input_ids, i, logits, options):
 
 
     #if repetition_penalty != 1: TODO: Need to figure out how to compare bool with jax parameters
-    logits = apply_repetition_penalty(  input_ids,
-                                         logits,
-                                         i,
-                                         repetition_penalty,
-                                         repetition_window,
-                                         repetition_penalty_normalize)
+    # logits = apply_repetition_penalty(  input_ids,
+    #                                      logits,
+    #                                      i,
+    #                                      repetition_penalty,
+    #                                      repetition_window,
+    #                                      repetition_penalty_normalize)
 
-        #penalties = _create_next_token_logits_penalties(input_ids, logits, repetition_penalty)
-        #logits = jnp.multiply(logits, penalties)
+    penalties = _create_next_token_logits_penalties(input_ids, logits, repetition_penalty)
+    logits = jnp.multiply(logits, penalties)
     return logits
