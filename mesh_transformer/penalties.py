0d020d8c98069882bc8a84849c20d01c80c4ee6f
JR Oakes <jroakes@gmail.com>
2021-08-04 12:59:21 -0400

    removed rep norm

--- mesh_transformer/penalties.py
+++ mesh_transformer/penalties.py
@@ -47,8 +47,11 @@ def apply_repetition_penalty(sequences,
                              jnp.ones_like(logits) * repetition_penalty,
                              jnp.ones_like(logits))
 
-    if repetition_penalty_normalize:
-        logits = jax.nn.log_softmax(logits)
+    # This throws an error.  I have no idea why
+    #if repetition_penalty_normalize:
+    #    logits = jax.nn.log_softmax(logits)
+
+    
     # Dividing a negative logit by the penalty tensor actually increases the
     # resulting probability. Take the inverse for negative logits.
     penalty_tensor = jnp.where(logits > 0,
