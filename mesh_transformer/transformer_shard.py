b38a8c20613b377dd3aa76db8101660d8b2b5db9
JR Oakes <jroakes@gmail.com>
2021-08-04 16:37:09 -0400

    reshaped next_token

--- mesh_transformer/transformer_shard.py
+++ mesh_transformer/transformer_shard.py
@@ -216,7 +216,7 @@ class CausalTransformer:
                     print('Next Token:')
                     print(next_token)
 
-                    new_context = jax.lax.dynamic_update_slice(context, [next_token],
+                    new_context = jax.lax.dynamic_update_slice(context, next_token.reshape(1,1),
                                              (0, i + 1))
 
                     if self.return_logits:
