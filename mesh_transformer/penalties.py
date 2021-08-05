2354a487285750d66689c86efa4e954c85897bea
JR Oakes <jroakes@gmail.com>
2021-08-04 22:11:14 -0400

    overcome jax._src.errors.ConcretizationTypeError during JIT compilation

--- mesh_transformer/penalties.py
+++ mesh_transformer/penalties.py
@@ -8,15 +8,18 @@ def _create_next_token_logits_penalties(input_ids, logits, repetition_penalty):
     print('input_ids', input_ids)
     print('logits', logits)
     print('Repetiton Penalty', repetition_penalty)
+    
+    if type(input_ids) != jax.interpreters.partial_eval.DynamicJaxprTracer:
+        prev_input_ids = jnp.unique(input_ids) # [[123,234,123,...]]
+        logit_penalized = logits[:, prev_input_ids]
+        logit_penalties = jnp.zeros(logit_penalized.shape)
+        # if previous logit score is < 0 then multiply repetition penalty else divide
+        logit_penalties[:, logit_penalized < 0] = repetition_penalty
+        logit_penalties[:, logit_penalized > 0] = 1 / repetition_penalty
 
-    prev_input_ids = jnp.unique(input_ids) # [[123,234,123,...]]
-    logit_penalized = logits[:, prev_input_ids]
-    logit_penalties = jnp.zeros(logit_penalized.shape)
-    # if previous logit score is < 0 then multiply repetition penalty else divide
-    logit_penalties[:, logit_penalized < 0] = repetition_penalty
-    logit_penalties[:, logit_penalized > 0] = 1 / repetition_penalty
+        logits - jnp.multiply(logits, logit_penalties)
 
-    return logit_penalties
+    return logits
 
 
 
@@ -26,5 +29,4 @@ def repetition_penalty(input_ids, i, logits, options):
     repetition_penalty = options.get('repetition_penalty', 1)
 
     penalties = _create_next_token_logits_penalties(input_ids, logits, repetition_penalty)
-    logits = jnp.multiply(logits, penalties)
     return logits
