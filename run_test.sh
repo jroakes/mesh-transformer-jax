56b79d96e355d5d7667b5d692953f4981ce11264
JR Oakes <jroakes@gmail.com>
2021-08-06 11:55:14 -0400

    Updated run text

--- run_test.sh
+++ run_test.sh
@@ -12,4 +12,4 @@ echo "Grabbing new test file"
 wget https://storage.googleapis.com/gpt-j-finetuning-europe/zoominfo_test.csv
 
 echo "Running sampling script"
-python3 device_sample_df.py --config=configs/company_data.json --sample_file zoominfo_test.csv --prompt_column model_input --name_column name --num_samples 100 --temp 0.75 --top_p 0.92 --rep_penalty 1.5
+python3 device_sample_df.py --config=configs/company_data.json --sample_file zoominfo_test.csv --prompt_column model_input --name_column name --num_samples 100 --temp 0.75 --top_p 0.90 --rep_penalty 1.3
