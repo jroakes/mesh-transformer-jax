92cbe1e3d542d62f7e55a9db3492ef2fa2e5d420
JR Oakes <jroakes@gmail.com>
2021-08-06 10:39:47 -0400

    I think I have repetition penatlty right now.

--- mesh_transformer/penalties.py
+++ mesh_transformer/penalties.py
@@ -3,21 +3,19 @@ import jax.numpy as jnp
 import numpy as np
 
 
-def _create_next_token_logits_penalties(input_ids, logits, repetition_penalty):
+def _create_next_token_logits_penalties(input_ids, logits, repetition_penalty, repetition_window):
 
-    print('input_ids', input_ids)
-    print('logits', logits)
-    print('Repetiton Penalty', repetition_penalty)
-    
-    if type(input_ids) != jax.interpreters.partial_eval.DynamicJaxprTracer:
-        prev_input_ids = jnp.unique(input_ids) # [[123,234,123,...]]
-        logit_penalized = logits[:, prev_input_ids]
-        logit_penalties = jnp.zeros(logit_penalized.shape)
-        # if previous logit score is < 0 then multiply repetition penalty else divide
-        logit_penalties[:, logit_penalized < 0] = repetition_penalty
-        logit_penalties[:, logit_penalized > 0] = 1 / repetition_penalty
+    prev_input_ids = input_ids[:, -repetition_window:].squeeze()
 
-        logits - jnp.multiply(logits, logit_penalties)
+    logit_penalized = logits[:, prev_input_ids]
+    logit_penalties = jnp.zeros(logit_penalized.shape)
+
+    # if previous logit score is < 0 then multiply repetition penalty else divide
+    logit_penalties = jnp.where(logit_penalized < 0, logit_penalties, repetition_penalty)
+    logit_penalties = jnp.where(logit_penalized > 0, logit_penalties, 1 / repetition_penalty)
+    token_penalties = jax.ops.index_update(token_penalties, jax.ops.index[:, prev_input_ids], logit_penalties)
+
+    logits = jnp.multiply(logits, token_penalties)
 
     return logits
 
@@ -27,6 +25,7 @@ def _create_next_token_logits_penalties(input_ids, logits, repetition_penalty):
 def repetition_penalty(input_ids, i, logits, options):
 
     repetition_penalty = options.get('repetition_penalty', 1)
+    repetition_window = options.get('repetition_window', 10)
 
-    penalties = _create_next_token_logits_penalties(input_ids, logits, repetition_penalty)
+    penalties = _create_next_token_logits_penalties(input_ids, logits, repetition_penalty, repetition_window)
     return logits
